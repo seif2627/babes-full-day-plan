8fb7236de0e0b38123c23f3f20208b82e8d4dd93
SAIF <saifaldeenalrifaie4@gmail.com>
2026-09-03 15:22:46 -0400

    Bump emergency plan cache after restaurant swap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='babes-plan-2026-09-03-v14';
+const CACHE='babes-plan-2026-09-03-v15';
 const CORE=['./','./index.html','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
